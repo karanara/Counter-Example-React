21a0f630400bd83784e880f88d2403ae2dae257e
karanara <karanara@mail.uc.edu>
2024-01-14 18:39:19 -0500

    added reset button

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -9,17 +9,23 @@ const Counter = ()=>{
     const decrementFunction=(by)=>{
         setCount(count-by);
     }
+    const resetCounter=()=>{
+        setCount(0);
+    }
     return (
         <div>
             <div className="text-center">
                 <div className="m-7">
-                <span className="text-lg font-bold">Count: <span  class="text-blue-500">{count}</span></span>
+                <span className="text-lg font-bold">Count: <span  className="text-blue-500">{count}</span></span>
                 </div>
             <div className="  justify-center ">
                 
              <AllInOneCounter by={1} increment={incrementFunction} decrement={decrementFunction}/>
             <AllInOneCounter by={3}  increment={incrementFunction} decrement={decrementFunction}/>
             <AllInOneCounter by={5}  increment={incrementFunction} decrement={decrementFunction}/>
+            <button className="bg-amber-600" 
+                        onClick={resetCounter}
+                >Reset</button>
             </div>    
             </div>
         </div>
